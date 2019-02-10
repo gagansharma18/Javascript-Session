ec474219be3738b42fc2c57771e54df78c594e4e
gagansharma18 <gagan.sharma28@gmail.com>
2019-02-10 12:05:52 +0530

    add Agenda

--- compilation.js
+++ compilation.js
@@ -1,3 +1,66 @@
+/*
+- SCOPE 
+    > Scope and the JavaScript Compiler
+    > Compiling Function Scope
+    > Execution of Function Code
+    > Scope and execution example
+    > Function Declarations, Function Expressions, and Block Scope
+    > Lexical Scope
+    > Cheating Lexical Scope: eval
+    > IIFE Pattern
+    > IIFE Pattern Questions
+    > Block Scope in ES6
+    > Problems with the Let Keyword
+    > Dynamic Scope
+    > Quiz: Scope
+    > Hoisting
+    > this keyword
+    > Binding confusion
+    > Explicit Binding
+    > the new Keyword
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+• Where to look for things (Variables and functions)
+• Who is doing the looking 
+• Introduce some compiler terminology (Misconception over interpreted/dynamic or complied) BASH is a interpreted language
+• JS compile code in at let 2 PASS Through the code.
+• Compiler is going to look for these blocks of scope
+• The Smallest atomic unit of scope that we have is function.(Not always true)
+
+- Compiling function scope
+- Execution of function code
+*/
+
+//DECELERATION IS WHEN YOU SEE "var" or "function"
+
+
 var foo = "bar";
 
 function bar() {
@@ -73,7 +136,10 @@ function baz(foo) {
 
 
 
+// NOTES
+/* so in pass 1 no matter how many times you write var foo it will declare only once
 
+*/
 
 
 
